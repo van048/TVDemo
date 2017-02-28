e8eaa401af32cebb139d0eed0cf68cfed9553c86
dqshare <739362790@qq.com>
2017-02-28 12:34:30 +0800

    local data fix Rxjava

--- app/src/main/java/cn/ben/tvdemo/data/tvtype/source/local/TVTypesLocalDataSource.java
+++ app/src/main/java/cn/ben/tvdemo/data/tvtype/source/local/TVTypesLocalDataSource.java
@@ -3,25 +3,19 @@ package cn.ben.tvdemo.data.tvtype.source.local;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
-import android.os.Handler;
-import android.os.HandlerThread;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.text.TextUtils;
 
-import com.squareup.sqlbrite.BriteDatabase;
-import com.squareup.sqlbrite.SqlBrite;
-
+import java.util.ArrayList;
 import java.util.List;
 
 import cn.ben.tvdemo.data.tvtype.TVTypes;
 import cn.ben.tvdemo.data.tvtype.source.TVTypesDataSource;
-import cn.ben.tvdemo.data.tvtype.source.TVTypesRepository;
 import cn.ben.tvdemo.util.schedulers.BaseSchedulerProvider;
-import hu.akarnokd.rxjava.interop.RxJavaInterop;
 import io.reactivex.Observable;
-import rx.functions.Action1;
-import rx.functions.Func1;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.functions.Consumer;
 
 import static cn.ben.tvdemo.data.tvtype.source.local.TVTypesPersistenceContract.TVTypeEntry.COLUMN_ID;
 import static cn.ben.tvdemo.data.tvtype.source.local.TVTypesPersistenceContract.TVTypeEntry.COLUMN_NAME;
@@ -29,29 +23,15 @@ import static cn.ben.tvdemo.data.tvtype.source.local.TVTypesPersistenceContract.
 import static com.google.common.base.Preconditions.checkNotNull;
 
 public class TVTypesLocalDataSource implements TVTypesDataSource {
-
-    @Nullable
     private volatile static TVTypesLocalDataSource instance = null;
-
-    @NonNull
+    private final TVTypesDbHelper mDbHelper;
+    private final SQLiteDatabase mReadableDatabase;
+    private final SQLiteDatabase mWritableDatabase;
     private final BaseSchedulerProvider mSchedulerProvider;
 
-    @NonNull
-    private final BriteDatabase mDatabaseHelper;
-
-    @NonNull
-    private final HandlerThread mDBThread;
-
-    @NonNull
-    private final Handler mDBHandler;
-
-    @NonNull
-    private final Func1<Cursor, TVTypes.TVType> mTVTypeMapperFunction = new Func1<Cursor, TVTypes.TVType>() {
+    private static final Consumer<? super Object> EMPTY_CONSUMER = new Consumer<Object>() {
         @Override
-        public TVTypes.TVType call(Cursor cursor) {
-            String id = cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_ID));
-            String name = cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_NAME));
-            return new TVTypes.TVType(id, name);
+        public void accept(Object o) throws Exception {
         }
     };
 
@@ -70,70 +50,95 @@ public class TVTypesLocalDataSource implements TVTypesDataSource {
     private TVTypesLocalDataSource(@NonNull Context context,
                                    @NonNull BaseSchedulerProvider schedulerProvider) {
         checkNotNull(context);
+
+        mDbHelper = new TVTypesDbHelper(context);
+        mReadableDatabase = mDbHelper.getReadableDatabase();
+        mWritableDatabase = mDbHelper.getWritableDatabase();
         mSchedulerProvider = checkNotNull(schedulerProvider);
-
-        TVTypesDbHelper dbHelper = new TVTypesDbHelper(context);
-        SqlBrite sqlBrite = new SqlBrite.Builder().build();
-        mDatabaseHelper = sqlBrite.wrapDatabaseHelper(dbHelper, mSchedulerProvider.io_1());
-
-        mDBThread = new HandlerThread(TVTypesRepository.class.getName());
-        mDBThread.start();
-        mDBHandler = new Handler(mDBThread.getLooper());
     }
 
     @Override
     public Observable<List<TVTypes.TVType>> getTVTypes() {
-        String[] projection = {
-                COLUMN_ID,
-                COLUMN_NAME
-        };
-        String sql = String.format("SELECT %s FROM %s", TextUtils.join(",", projection), TABLE_NAME);
-        return RxJavaInterop.toV2Observable(
-                mDatabaseHelper
-                        .createQuery(TABLE_NAME, sql)
-                        .mapToList(mTVTypeMapperFunction)
-        );
+        return Observable
+                .create(new ObservableOnSubscribe<List<TVTypes.TVType>>() {
+                    @Override
+                    public void subscribe(ObservableEmitter<List<TVTypes.TVType>> e) throws Exception {
+                        final List<TVTypes.TVType> tvTypes = new ArrayList<>();
+
+                        String[] projection = {
+                                COLUMN_ID,
+                                COLUMN_NAME
+                        };
+
+                        Cursor cursor = mReadableDatabase.query(TABLE_NAME, projection, null, null, null, null, null);
+                        if (cursor != null) {
+                            while (cursor.moveToNext()) {
+                                String id = cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_ID));
+                                String name = cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_NAME));
+                                TVTypes.TVType tvType = new TVTypes.TVType(id, name);
+                                tvTypes.add(tvType);
+                            }
+                        }
+                        if (cursor != null) {
+                            cursor.close();
+                        }
+
+                        e.onNext(tvTypes);
+                    }
+                });
     }
 
     @Override
     public void deleteAllTVTypes() {
-        mDBHandler.post(new Runnable() {
-            @Override
-            public void run() {
-                mDatabaseHelper.delete(TABLE_NAME, null);
-            }
-        });
+        Observable
+                .create(new ObservableOnSubscribe<Object>() {
+                    @Override
+                    public void subscribe(ObservableEmitter<Object> e) throws Exception {
+                        mWritableDatabase.delete(TABLE_NAME, null, null);
+                    }
+                })
+                .subscribeOn(mSchedulerProvider.io())
+                .subscribe(EMPTY_CONSUMER);
     }
 
     @Override
     public void saveTVType(@NonNull final TVTypes.TVType tvType) {
         checkNotNull(tvType);
 
-        // make sure only each entry has unique ID
-        String sql = String.format("SELECT * FROM %s WHERE %s=?", TABLE_NAME, COLUMN_ID);
-        mDatabaseHelper.
-                createQuery(TABLE_NAME, sql, tvType.getId())
-                .mapToList(mTVTypeMapperFunction)
-                .observeOn(mSchedulerProvider.io_1())
-                .subscribe(new Action1<List<TVTypes.TVType>>() {
+        Observable
+                .create(new ObservableOnSubscribe<Object>() {
                     @Override
-                    public void call(List<TVTypes.TVType> tvTypes) {
-                        if (!tvTypes.isEmpty()) {
+                    public void subscribe(ObservableEmitter<Object> e) throws Exception {
+                        // make sure only each entry has unique ID
+                        Cursor cursor = mReadableDatabase.query(
+                                TABLE_NAME,
+                                new String[]{COLUMN_ID},
+                                COLUMN_ID + "=?",
+                                new String[]{tvType.getId()}, null, null, null);
+                        if (cursor != null && cursor.getCount() > 1) {
+                            cursor.close();
+                            mReadableDatabase.close();
                             throw new AssertionError("Duplicated TV Type ID");
-                        } else {
-                            ContentValues contentValues = new ContentValues();
-                            contentValues.put(COLUMN_ID, tvType.getId());
-                            contentValues.put(COLUMN_NAME, tvType.getName());
-                            mDatabaseHelper.insert(TABLE_NAME, contentValues);
                         }
+                        if (cursor != null) {
+                            cursor.close();
+                        }
+
+                        ContentValues contentValues = new ContentValues();
+                        contentValues.put(COLUMN_ID, tvType.getId());
+                        contentValues.put(COLUMN_NAME, tvType.getName());
+                        mWritableDatabase.insert(TABLE_NAME, null, contentValues);
                     }
-                });
+                })
+                .subscribeOn(mSchedulerProvider.io())
+                .subscribe(EMPTY_CONSUMER);
     }
 
     @Override
     public void cleanup() {
-        mDBThread.quitSafely();
-        mDatabaseHelper.close();
+        mReadableDatabase.close();
+        mWritableDatabase.close();
+        mDbHelper.close();
         instance = null;
     }
 }
